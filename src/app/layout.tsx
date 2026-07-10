0afce60f78e26c32774ca5a09a905948e5421b2b
kuldeepsharma1 <hytek112@gmail.com>
2026-07-10 21:47:44 +0530

    feat: enhance styling and components
    - Updated globals.css to include new theme variables and dark mode styles.
    - Added Toaster component for notifications using Sonner.
    - Created Button component with variants for different styles.
    - Introduced utility function `cn` for merging class names.
    - Updated layout.tsx to include Toaster for global notifications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner"
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -33,6 +35,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+            <Toaster />
       </body>
     </html>
   );
